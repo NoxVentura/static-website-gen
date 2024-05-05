4290f80129a39298121ae7cbc20922f4a29a6201
NoxVentura <jupiterthekingpin@gmail.com>
2024-05-06 01:01:33 +0530

    Well a lot has happened.

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+def main():
+    mytextnode = TextNode('This is a piece of text',"bold")
+    print(f"{mytextnode} is the text node we've just created")
+
+if __name__ == '__main__':
+    main()
+
