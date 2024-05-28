8b4f3364d3cd8b776474e6bbd9ad6faf151baa58
NoxVentura <jupiterthekingpin@gmail.com>
2024-05-28 18:36:44 +0530

    Nodes done

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,5 @@
 class HtmlNode:
-    def __init__(self, tag = None, value = None, children = None, props = None):
+    def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
         self.children = children
@@ -7,7 +7,7 @@ class HtmlNode:
 
     def to_html(self):
         raise NotImplementedError
-    
+
     def props_to_html(self):
         if self.props is None:
             return ""
@@ -15,14 +15,15 @@ class HtmlNode:
         for prop in self.props:
             props_html += f' {prop}="{self.props[prop]}"'
         return props_html
-    
+
     def __repr__(self):
         return f"HtmlNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
 
+
 class LeafNode(HtmlNode):
-    def __init__(self, tag, value, children = None, props = None):
-        super().__init__(tag, value, None , props)
-    
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
     def to_html(self):
         if self.value is None:
             raise ValueError
@@ -33,13 +34,14 @@ class LeafNode(HtmlNode):
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props}"
 
-class ParentNode(HtmlNode):
-    def __init__(self, tag, children, props = None):
-        super().__init__(tag, children, props)
-    
-    def to_html(self):
-        if tag is None:
-            raise ValueError("No tag found")
-        if children is None:
-            raise ValueError("No children found")
 
+class ParentNode(HtmlNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("No tag found")
+        if self.children is None:
+            raise ValueError("No children found")
+        return f"<{self.tag}{self.props_to_html()}>{''.join(child.to_html() for child in self.children)}</{self.tag}>"
